e9b1fd0ba7d5f3d59fa313dacd049078c621ec7f
lonnen <chris.lonnen@gmail.com>
2020-03-29 20:42:26 -0700

    WIP -- pausing here

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
+extern crate ring;
+
 use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
+
+use ring::hmac::Hmac;
+use ring::sha1::Sha1;
 
 const counterLen: usize = 20;
 struct Keychain {
     file: String,
     data: Vec<u8>,
     keys: HashMap<String, Key>,
-}
-
-struct Key {
-    raw: Vec<bool>,
-    digits: usize,
-    offset: usize,
 }
 
 impl Keychain {
@@ -33,16 +32,44 @@ impl Keychain {
     //     key.code(self.data);
     //     &String::from("asdf")
     // }
+}
 
+struct Key {
+    raw: Vec<u8>,
+    digits: usize,
+    offset: usize,
 }
 
 impl Key {
-    pub fn new() -> Key {
-        Key {
+    /// Create an empty `Key`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use oxy2fa::Key;
+    ///
+    /// let k: Key = Key::new();
+    ///
+    ///
+    /// ```
+    pub fn new(raw: Vec<u8>, digits: usize, offset: usize) -> Option<Key> {
+        // digits should be restricted to 6 and 9.3 digits (can be 0, 1, 2, 3) (log 31 b10)
+        match digits {
+            6 | 7 | 8 | 9 | 10 => (),
+            _ => return None,
+        }
+
+        let hmac_key = vec![32; 0u8];
+
+        let mut hmac = Hmac::new(Sha1::new(), hmac_key.as_slice());
+        hmac.input(message);
+        hmac.result().code().to_hex();
+
+        Some(Key {
             raw: vec![],
             digits: 0,
             offset: 0,
-        }
+        })
     }
 
     // pub fn code<'a>(&'a self, _n: Vec<u8>) -> &'a String {
